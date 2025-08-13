4aa3b631df6f754267e30ef4078d1a50ef5a1170
Arpit <arpitsengar99@gmail.com>
2025-08-13 13:52:09 +0530

    init server

---
+++ server/main.py
@@ -0,0 +1,13 @@
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+if __name__=="__main__":
+    uvicorn.run("main:app", port=7860, host="0.0.0.0")
